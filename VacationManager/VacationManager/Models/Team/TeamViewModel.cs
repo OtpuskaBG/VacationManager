1e33aa70ad6e79dac1a365debdbe5c74b67eaca4
Stef40IT <stef40tobg@gmail.com>
2025-04-05 22:24:41 +0300

    models and profiles folders

---
+++ VacationManager/VacationManager/Models/Team/TeamViewModel.cs
@@ -0,0 +1,17 @@
+﻿using VacationManager.Data.Models;
+
+namespace VacationManager.Models.Team
+{
+    public class TeamViewModel
+    {
+        public string Name { get; set; } = "";
+
+
+        //public Guid ProjectId { get; set; }
+        public Project Project { get; set; }
+
+
+
+        public List<ApplicationUser> Developers { get; set; } = new();
+    }
+}
---
+++ VacationManager/VacationManager/Profiles/TeamProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using VacationManager.Core.Prototypes;
+using VacationManager.Data.Models;
+using VacationManager.Models.Team;
+namespace VacationManager.Profiles
+{
+    public class TeamProfile : Profile
+    {
+        public TeamProfile()
+        {
+            this.CreateMap<TeamViewModel, Team>();
+            this.CreateMap<TeamViewModel, TeamPrototype>();
+            this.CreateMap<Team, TeamViewModel>();
+            this.CreateMap<Team, TeamPrototype>();
+            this.CreateMap<VacationManager.Data.Models.Team, VacationManager.Components.Pages.Team.Team>();
+        }
+    }
+}
