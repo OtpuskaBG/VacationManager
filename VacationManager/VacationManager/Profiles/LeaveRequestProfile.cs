c3ee06356272f6ee28341c5caf06904007981c33
Niko Nikolov <nikonikolov2006@gmail.com>
2025-04-09 22:27:31 +0300

    Reapply "Added the pages for the leave requests. Needs further fixing and enhancements."
    This reverts commit 1911bd2031696fcce292fd7e7f87088397b5952e.

---
+++ VacationManager/VacationManager/Profiles/LeaveRequestProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using VacationManager.Data.Models;
+using VacationManager.Core.Prototypes;
+using VacationManager.Models.LeaveRequest;
+
+namespace VacationManager.Profiles
+{
+    public class LeaveRequestProfile : Profile
+    {
+        public LeaveRequestProfile()
+        {
+            //TODO: Further expand the mapper.
+
+            this.CreateMap<LeaveRequest, LeaveRequestViewModel>();
+            this.CreateMap<LeaveRequestPrototype, LeaveRequest>();
+        }
+    }
+}
