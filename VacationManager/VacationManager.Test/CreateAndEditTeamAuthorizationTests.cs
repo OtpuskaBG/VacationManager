8f8694f05410efbd1fca17cc0b63b5973242174c
Niko Nikolov <nikonikolov2006@gmail.com>
2025-04-12 16:09:44 +0300

    Created tests

---
+++ VacationManager/VacationManager.Test/CreateAndEditTeamAuthorizationTests.cs
@@ -0,0 +1,139 @@
+﻿using System.Reflection;
+using System.Threading;
+using Microsoft.AspNetCore.Http;
+using Moq;
+using MudBlazor.Services;
+using VacationManager.Core.Authentication;
+using VacationManager.Core.Authentication.Abstractions;
+using VacationManager.Core.Services.Abstractions;
+using VacationManager.Data;
+using VacationManager.Data.Repositories;
+using VacationManager.Data.Repositories.Abstractions;
+using VacationManager.Profiles;
+using VacationManager.Components.Pages.Leave_Requests;
+using VacationManager.Data.Models;
+using Xunit;
+using Bunit.TestDoubles;
+using Microsoft.Extensions.DependencyInjection;
+using VacationManager.Core.Services;
+using VacationManager.Data.Enums;
+using Assert = Xunit.Assert;
+using Microsoft.AspNetCore.Cors.Infrastructure;
+
+namespace VacationManager.Test;
+
+public class CreateAndEditTeamAuthorizationTests : Bunit.TestContext
+{
+    private readonly TestAuthorizationContext _authContext;
+
+    private CreateAndEditTeamAuthorizationTests()
+    {
+        Services.AddMudServices();
+        Assembly currentAssembly = Assembly.GetExecutingAssembly();
+        Services.AddAutoMapper(currentAssembly);
+        Services.AddAutoMapper(typeof(TeamProfile));
+        Services.AddAutoMapper(typeof(ProjectProfile));
+        Services.AddScoped<IAuthenticationContext, AuthenticationContext>();
+        Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+
+        var mockCourseService = new Mock<IProjectService>();
+        mockCourseService
+            .Setup(service => service.GetByIdAsync(It.IsAny<Guid>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync((Project?)new()); // Simulate "course not found"
+        Services.AddScoped(_ => mockCourseService.Object);
+
+        var mockModuleService = new Mock<ITeamService>();
+        mockModuleService
+            .Setup(service => service.GetByIdAsync(It.IsAny<Guid>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync((VacationManager.Data.Models.Team?)new()); // Simulate "module not found"
+        Services.AddScoped(_ => mockModuleService.Object);
+
+        Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+        Services.AddDbContext<ApplicationDbContext>();
+
+        _authContext = this.AddTestAuthorization();
+        _authContext.SetAuthorized("testuser");
+    }
+
+    [Fact]
+    public void CEOCanAccessCreateTeam()
+    {
+        _authContext.SetRoles(Role.CEO.ToString());
+
+        var cut = RenderComponent<VacationManager.Components.Pages.Team.Create>();
+
+        Assert.NotEqual("", cut.Markup);
+        Assert.Equal("", cut.Markup);
+    }
+
+    [Fact]
+    public void CEOCanAccessEditTeam()
+    {
+        _authContext.SetRoles(Role.CEO.ToString());
+
+        var cut = RenderComponent<VacationManager.Components.Pages.Team.Edit>();
+
+        Assert.NotEqual("", cut.Markup);
+        Assert.Equal("", cut.Markup);
+    }
+
+    [Fact]
+    public void TeamLeadCannotAccessCreateTeam()
+    {
+        _authContext.SetRoles(Role.TeamLead.ToString());
+
+        var cut = RenderComponent<VacationManager.Components.Pages.Team.Create>();
+
+        Assert.Equal("", cut.Markup);
+    }
+
+    [Fact]
+    public void TeamLeadCannotAccessEditTeam()
+    {
+        _authContext.SetRoles(Role.TeamLead.ToString());
+
+        var cut = RenderComponent<VacationManager.Components.Pages.Team.Edit>();
+
+        Assert.Equal("", cut.Markup);
+    }
+
+    [Fact]
+    public void DeveloperCannotAccessCreateTeam()
+    {
+        _authContext.SetRoles(Role.Developer.ToString());
+
+        var cut = RenderComponent<VacationManager.Components.Pages.Team.Create>();
+
+        Assert.Equal("", cut.Markup);
+    }
+
+    [Fact]
+    public void DeveloperCannotAccessEditTeam()
+    {
+        _authContext.SetRoles(Role.Developer.ToString());
+
+        var cut = RenderComponent<VacationManager.Components.Pages.Team.Edit>();
+
+        Assert.Equal("", cut.Markup);
+    }
+
+    [Fact]
+    public void UnassignedCannotAccessCreateTeam()
+    {
+        _authContext.SetRoles(Role.Unassigned.ToString());
+
+        var cut = RenderComponent<VacationManager.Components.Pages.Team.Create>();
+
+        Assert.Equal("", cut.Markup);
+    }
+
+    [Fact]
+    public void UnassignedCannotAccessEditTeam()
+    {
+        _authContext.SetRoles(Role.Unassigned.ToString());
+
+        var cut = RenderComponent<VacationManager.Components.Pages.Team.Edit>();
+
+        Assert.Equal("", cut.Markup);
+    }
+}
